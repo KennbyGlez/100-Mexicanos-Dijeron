9f8b7c60b8b2cef5003c8a318d9c1453716a468e
KennbyGlez <kennbybgonzalez@gmail.com>
2026-05-26 17:42:48 -0700

    Configurado gitignore para Electron

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+
+    autoHideMenuBar: true,
+
+    webPreferences: {
+      contextIsolation: true,
+    },
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
